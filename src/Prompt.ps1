1abc6391fda277cc780b18c27fad6c66fa87e5b9
Aryan <aryanebrahimpour@gmail.com>
2019-01-04 01:17:17 +0330

    Excess command

--- src/Prompt.ps1
+++ src/Prompt.ps1
@@ -1,7 +1,6 @@
 # Overrides prompt
 
 function prompt {
-    $Host.UI.RawUI.BackgroundColor = ($bckgrnd = 'Black')
     $p = Split-Path -leaf -path (Get-Location)
     if((pwd).Path.Length -eq 3) {"$p> "}
     else {"~/$p> "}
