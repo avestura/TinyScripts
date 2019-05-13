b1be13575ed674f3e623fcd93dbb900a089c58be
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-05-13 22:56:03 +0430

    Update Start-Tor.ps1

--- src/Start-Tor.ps1
+++ src/Start-Tor.ps1
@@ -26,7 +26,7 @@ function Start-Tor {
     $Tor_Version       = Get-OneToLastItem($TOR_VERSION_RAW)
 
     Write-Host "Running $Tor_Version" -ForegroundColor DarkGray
+    Write-Host "Press [Ctrl+C] to stop Tor service."
     & "$torExe" --defaults-torrc $torrc_defaults -f $torrc DataDirectory $tordata GeoIPFile $geoIP GeoIPv6File $geoIPv6 HashedControlPassword $Tor_HashPass +__ControlPort $CTRL_PORT +__SocksPort "${TOR_HOST}:$TOR_PORT IPv6Traffic PreferIPv6 KeepAliveIsolateSOCKSAuth" __OwningControllerProcess $controllerProcess | more
-    Write-Host "Tor.exe invoked."
-    Write-Host "If the configuration was correct, Tor will work while this window is open."
+    
 }
