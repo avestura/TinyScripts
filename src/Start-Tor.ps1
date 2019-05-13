96cba634a9d65665aa0ee217844ab2d8d81d3295
Aryan Ebrahimpour <aryanebrahimpour@gmail.com>
2019-05-13 22:33:16 +0430

    Update Start-Tor.ps1

--- src/Start-Tor.ps1
+++ src/Start-Tor.ps1
@@ -9,12 +9,26 @@ function Start-Tor {
     $geoIP          = "$torBrowser\Browser\TorBrowser\Data\Tor\geoip"
     $geoIPv6        = "$torBrowser\Browser\TorBrowser\Data\Tor\geoip6"
 
+    $torExe         = "$tor_location\tor.exe"
+
     $controllerProcess = $PID
     $TOR_HOST          = "127.0.0.1"
-    $TOR_PORT          = "9150"
+    $TOR_PORT          = 9150
+    $CTRL_PORT         = 9151
 
-    Write-Host "Running Tor in command-line..." -ForegroundColor DarkGray
-    & "$tor_location\tor.exe" --defaults-torrc $torrc_defaults -f $torrc DataDirectory $tordata GeoIPFile $geoIP GeoIPv6File $geoIPv6 HashedControlPassword 16:f132bb74458f049260d2aae9c1cdcbc7d74b371e76a5c68598cc6de5c4 +__ControlPort 9151 +__SocksPort "${TOR_HOST}:$TOR_PORT IPv6Traffic PreferIPv6 KeepAliveIsolateSOCKSAuth" __OwningControllerProcess $controllerProcess
-    Write-Host "Tor is now running. Tor will work while this window is open." -ForegroundColor Green
+    $TOR_Password      = "YourPasswordHere"
 
+    function Get-OneToLastItem { param ($arr) return $arr[$arr.Length - 2]}
+
+    Write-Host "Generating hash for your Tor password..."
+    $TOR_HashPass_RAW  = & "$torExe" --hash-password $TOR_Password | more
+    $Tor_HashPass      = Get-OneToLastItem($TOR_HashPass_RAW)
+
+    $TOR_VERSION_RAW   = & "$torExe" --version | more
+    $Tor_Version       = Get-OneToLastItem($TOR_VERSION_RAW)
+
+    Write-Host "Running $Tor_Version" -ForegroundColor DarkGray
+    & "$torExe" --defaults-torrc $torrc_defaults -f $torrc DataDirectory $tordata GeoIPFile $geoIP GeoIPv6File $geoIPv6 HashedControlPassword $Tor_HashPass +__ControlPort $CTRL_PORT +__SocksPort "${TOR_HOST}:$TOR_PORT IPv6Traffic PreferIPv6 KeepAliveIsolateSOCKSAuth" __OwningControllerProcess $controllerProcess | more
+    Write-Host "Tor.exe invoked successfully."
+    Write-Host "If the configuration was correct, Tor will work while this window is open." -ForegroundColor Green
 }
